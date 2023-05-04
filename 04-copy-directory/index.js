fac43a5b95713277043165830eee40e66bb3b47a
eordan <pistsouandrey@gmail.com>
2023-05-04 22:34:08 +0300

    feat: fourth task '04-copy-directory' is implemeneted

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+function copyDir(src, dest) {
+  fs.mkdirSync(dest, { recursive: true });
+
+  fs.readdirSync(src).forEach(file => {
+    const srcPath = path.join(src, file);
+    const destPath = path.join(dest, file);
+
+    if (fs.statSync(srcPath).isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  });
+}
+
+copyDir(srcDir, destDir);
