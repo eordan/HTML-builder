47ea88310f52b943a1dd728668b238a62668d3de
eordan <pistsouandrey@gmail.com>
2023-05-07 19:19:08 +0300

    fix: rewtite fourth task '04-copy-directory' using only asynchronous functions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,42 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const srcDir = path.join(__dirname, 'files');
 const destDir = path.join(__dirname, 'files-copy');
 
-function copyDir(src, dest) {
-  fs.mkdirSync(dest, { recursive: true });
+async function copyDir(src, dest) {
+  await fs.mkdir(dest, { recursive: true });
 
-  fs.readdirSync(src).forEach(file => {
+  const files = await fs.readdir(src);
+  for (const file of files) {
     const srcPath = path.join(src, file);
     const destPath = path.join(dest, file);
 
-    if (fs.statSync(srcPath).isDirectory()) {
-      copyDir(srcPath, destPath);
+    const stats = await fs.stat(srcPath);
+    if (stats.isDirectory()) {
+      await copyDir(srcPath, destPath);
     } else {
-      fs.copyFileSync(srcPath, destPath);
+      await fs.copyFile(srcPath, destPath);
     }
-  });
+  }
 }
 
-copyDir(srcDir, destDir);
+async function run() {
+  try {
+    // Check if destDir exists and remove it if it does
+    const destExists = await fs.stat(destDir).then(() => true).catch(() => false);
+    if (destExists) {
+      console.log(`Removing ${destDir}`);
+      await fs.rm(destDir, { recursive: true });
+    }
+
+    // Create a new destDir and copy the files
+    console.log(`Copying ${srcDir} to ${destDir}`);
+    await copyDir(srcDir, destDir);
+    console.log('Copy completed successfully!');
+  } catch (err) {
+    console.error('Error copying directory:', err);
+  }
+}
+
+run();
